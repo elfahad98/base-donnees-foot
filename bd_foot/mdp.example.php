442e3d414af6686330ecd45882d4c84be22f94cb
elfahad98 <elfahad180@gmail.com>
2025-10-28 13:36:04 +0100

    Update and rename mdp.php to mdp.example.php

---
+++ bd_foot/mdp.example.php
@@ -0,0 +1,5 @@
+<?php
+    // Exemple : fichier de mot de passe 
+    $mdp = "votre_mot_de_passe";
+	
+?>
--- bd_foot/mdp.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // Exemple : fichier de mot de passe (ne pas versionner le vrai )
-    $mdp = "votre_mot_de_passe";
-	
-?>
