880cb98575cfd47088b98e04762fa8b3085da64b
elfahad98 <elfahad180@gmail.com>
2025-10-28 13:32:55 +0100

    Update pgsql.php

--- bd_foot/pgsql.php
+++ bd_foot/pgsql.php
@@ -1,14 +1,13 @@
 <?php
-	include("mdp.php");
-	
-	$serveur="postgresql-etu.unicaen.fr";
-	$login="combo211";
-	$base="combo211_bd";
-	$connexion="host=$serveur dbname=$base user=$login password=$mdp";
-	$id_connexion=pg_connect($connexion) or die('Connexion impossible :'.pg_last_error());
+    // Exemple de configuration PostgreSQL
+    // Remplacez les champs ci-dessous par vos propres identifiants
 
+    $serveur = "postgresql-etu.unicaen.fr";
+    $login = "votre_login";
+    $base = "votre_base";
+    $mdp = "votre_mot_de_passe";
 
-	if (!$id_connexion) {
-		die('Connexion impossible :' . pg_last_error());
-	}
+    $connexion = "host=$serveur dbname=$base user=$login password=$mdp";
+    $id_connexion = pg_connect($connexion) or die('Connexion impossible :' . pg_last_error());
 ?>
+
